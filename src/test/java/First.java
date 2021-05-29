9117ce3c4d94e11511a93754ce18439d0b695a3b
Deum <oscar.sdet@gmail.com>
2021-05-28 22:45:42 -0400

    set up ,

---
+++ src/test/java/First.java
@@ -0,0 +1,11 @@
+public class First {
+    public static void main(String[] args) {
+        System.out.println("args = " + args);
+    }
+    /**
+     * hello
+     * hello
+     * hello
+     *
+     */
+}
